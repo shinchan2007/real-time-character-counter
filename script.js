3ecdd1bfa03d4fd51551ccf350d1d51e1c7de4c8
KISHOR <kishorsuresh210@gmail.com>
2024-11-08 18:06:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let div = document.getElementById("bottomdiv");
 input.addEventListener("input",()=>{
     //console.log(input.value);
     let current = input.value;
-    console.log(current.length);
     div.innerHTML =`<h2 style='margin-left:20px;margin-top:50px;'>Total Characters:<span style="color: purple;">${current.length}</span></h2>
     <h2 style='margin-right:20px;margin-top:50px;'>Remaining:<span style="color: red;">${50-current.length}</span></h2>`;
 
